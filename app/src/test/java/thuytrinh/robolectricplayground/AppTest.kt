291824eb71808af78efab8afa371eb8c64df8ede
Thuy Trinh <thuy@fastmail.com>
2020-09-22 12:02:40 +0200

    Write a placeholder test

---
+++ app/src/test/java/thuytrinh/robolectricplayground/AppTest.kt
@@ -0,0 +1,15 @@
+package thuytrinh.robolectricplayground
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class AppTest {
+  @Test
+  fun `works fine`() {
+    // Given
+    // When
+    // Then
+  }
+}
