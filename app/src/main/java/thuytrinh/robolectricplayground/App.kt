d736e480685faeaac41ff825977e542626a36823
Thuy Trinh <thuy@fastmail.com>
2020-09-22 12:29:41 +0200

    Attempt to initialize WorkManager

--- app/src/main/java/thuytrinh/robolectricplayground/App.kt
+++ app/src/main/java/thuytrinh/robolectricplayground/App.kt
@@ -1,9 +1,12 @@
 package thuytrinh.robolectricplayground
 
 import android.app.Application
+import androidx.work.WorkManager
 
 class App : Application() {
   override fun onCreate() {
     super.onCreate()
+
+    val workManager = WorkManager.getInstance(this)
   }
 }
